88e5bfc65a7480f5178f45c43dc6de2bc88036e6
Lapingold <daniel.lapington@chasacademy.se>
2024-03-18 07:33:58 +0100

    made changes in cover_it.ino, psensor works

--- cover_it.ino
+++ cover_it.ino
@@ -14,7 +14,6 @@ bool cover_it(uint8_t coverLed, uint8_t photoPin, long timeout)
       return true;
     }
   }
-
   // timeout reached
   digitalWrite(coverLed, 0);
   return false;
@@ -22,28 +21,13 @@ bool cover_it(uint8_t coverLed, uint8_t photoPin, long timeout)
 
 static bool read_photosensor(uint8_t photoPin)
 {
-  const int count = 10;
-  int readings[count];
-  int total = 0;
-  int avg = 0;
+  int lightRaw = analogRead(photoPin);
+  int light = map(lightRaw, 1023, 0, 10, 0);
 
-  static int psensor_min = 1 << 14 - 1;
-  static int psensor_max = 0;
-
-  analogRead(photoPin); // stabilise sensor reading
-  for (int i = 0; i < count; ++i) {
-    readings[i] = analogRead(photoPin); // take average reading
-    total += readings[i];
+  if (light < 5) {
+    return true;
   }
-
-  avg = total / count;
-
-  int value = avg; // analogRead(photoPin);
-  psensor_max = max(value, psensor_max);
-  psensor_min = min(value, psensor_min);
-  int diff = psensor_max - psensor_min;
-
-  // Threshold is half the diff of max and min
-  int threshold = diff / 2;
-  return (diff - (value - psensor_min)) > threshold;
+  else {
+    return false;
+  }
 }
